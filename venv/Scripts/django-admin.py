f096fbfb302c89f81bc374dd9046451b7272af2d
clopezlopez <49844668+clopezlopez@users.noreply.github.com>
2020-06-21 17:37:39 -0400

    First Commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\camil\documents\vscode_projects\personal_portfolio_project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
